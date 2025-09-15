3dacdcc1c3fe972f0091a8bf3473872ee43770d9
cody-raves <166961458+cody-raves@users.noreply.github.com>
2025-09-14 20:48:36 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
 
 author 'cody-raves'
-description 'cr-pause nopixel inspired pause menu + NV timecycle'
-version '1.1.0'
+description 'cr-pause nopixel inspired pause menu'
+version '1.0.0'
 
 client_scripts {
     'config.lua',
@@ -13,9 +14,9 @@ client_scripts {
 files {
     'html/pausemenu.html',
     'html/logo.png',
-    'stream/timecycle_cr_pause.xml'
+    'stream/timecycle_cr_pause.xml'   -- <- one path, matches the actual file
 }
 
-data_file 'TIMECYCLEMOD_FILE' 'stream/timecycle_cr_pause.xml'
+data_file 'TIMECYCLEMOD_FILE' 'stream/timecycle_cr_pause.xml'  -- <- same path
 
 ui_page 'html/pausemenu.html'
